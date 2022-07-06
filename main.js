0d273d8b12b032d4a34c6681b01ce4f3f46218df
Mariana Abdias Gonçalves <mabdiasgoncalves@gmail.com>
2022-07-06 20:56:32 -0300

    criei uma página de detalhes e deixei responsivo

--- main.js
+++ main.js
@@ -15,11 +15,7 @@ const fetchPokemon = () => {
 
       accumulator += `
                       <li class="card ${types[0]}">
-                      <img class="card-image" alt="${
-                        pokemon.name
-                      }" src="https://raw.githubusercontent.com/PokeApi/sprites/master/sprites/pokemon/${
-        pokemon.id
-      }.png">
+                      <img class="card-image" alt="${pokemon.name}" src="https://raw.githubusercontent.com/PokeApi/sprites/master/sprites/pokemon/${pokemon.id}.png">
                       <h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
                       <p class="card-subtitle">${types.join(' | ')} </p>
                       </li>`
